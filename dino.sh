ad7737b9216da32c57fcf6b1f3493e459db789b2
Jonas Stettner <mail@jstet.net>
2023-07-21 23:59:48 +0200

    integrate myrtle

--- dino.sh
+++ dino.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Function to check internet connectivity
 check_internet() {
   if ping -q -c 1 -W 1 google.com >/dev/null; then
     return 0
@@ -9,16 +8,12 @@ check_internet() {
   fi
 }
 
-# Check internet connectivity
 if check_internet; then
   # Make the API request and store the response
-  response=$(curl -s "https://api.quotable.io/quotes/random")
-
-  # Extract the content from the response using a JSON parsing tool like 'jq'
-  content=$(echo "$response" | jq -r '.[0].content')
+  response=$(curl -s "https://myrtle.jstet.net/")
 
   # Pipe the content into dinosay and apply lolcat
-  dinosay -d brachio "$content"| lolcat
+  echo "$response" | lolcat
 else
-  echo "No internet connection."
+  echo "No internet connection." | lolcat
 fi
